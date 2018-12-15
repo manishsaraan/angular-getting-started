08072fd612f5ed3e444486e7ec7d967c21455a73
manishsaraan <manish021js@gmail.com>
2018-12-15 15:43:34 +0530

    passing data to recipe details implemented

--- shopping-app/src/app/recipes/recipes-list/recipes-list.component.ts
+++ shopping-app/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipes-list',
@@ -6,13 +6,18 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test', 'https://via.placeholder.com/150'),
-    new Recipe('A Test Recipe', 'This is a test', 'https://via.placeholder.com/150'),
+    new Recipe('Another Recipe', 'This is a test', 'https://via.placeholder.com/150'),
   ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
