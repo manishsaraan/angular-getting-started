434bf9c9b7720a7fbc0725cae4520617d0b51956
manishsaraan <manish021js@gmail.com>
2018-12-20 17:31:30 +0530

    basic directive implemented

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BasicHightlightDirective } from '../app/basic-highlight/basic-highlight.directive';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHightlightDirective
   ],
   imports: [
     BrowserModule,
