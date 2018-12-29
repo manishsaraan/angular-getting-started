5b10491f3d855442e58f6bd1affa700982b7efb1
manishsaraan <manish021js@gmail.com>
2018-12-29 15:58:21 +0530

    account events movedd go account service

--- services-start/src/app/app.component.ts
+++ services-start/src/app/app.component.ts
@@ -1,31 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './accounts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [AccountService]
 })
-export class AppComponent {
-  accounts = [
-    {
-      name: 'Master Account',
-      status: 'active'
-    },
-    {
-      name: 'Testaccount',
-      status: 'inactive'
-    },
-    {
-      name: 'Hidden Account',
-      status: 'unknown'
-    }
-  ];
+export class AppComponent implements OnInit {
 
-  onAccountAdded(newAccount: {name: string, status: string}) {
-    this.accounts.push(newAccount);
+  accounts = [];
+  constructor(private accountsService: AccountService) {
+
   }
 
-  onStatusChanged(updateInfo: {id: number, newStatus: string}) {
-    this.accounts[updateInfo.id].status = updateInfo.newStatus;
+  ngOnInit() {
+    console.log("-accounts", this.accountsService.accounts)
+    this.accounts = this.accountsService.accounts;
   }
 }
