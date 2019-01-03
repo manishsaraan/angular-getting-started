905a6a4d097ea05e339da1df66397190a5b01caa
manishsaraan <manish021js@gmail.com>
2019-01-03 13:45:15 +0530

    Revert "shopping component events moved to recipe service"
    This reverts commit 4fc484e7bffe45c7bfb4ed0215b042f2317d1b00.

--- shopping-app/src/app/recipes/recipe.service.ts
+++ shopping-app/src/app/recipes/recipe.service.ts
@@ -1,8 +1,6 @@
 import { Recipe } from './recipe.model';
-import { EventEmitter } from 'events';
 
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is a test', 'https://via.placeholder.com/150'),
         new Recipe('Another Recipe', 'This is a test', 'https://via.placeholder.com/150'),
--- shopping-app/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
+++ shopping-app/src/app/recipes/recipes-list/recipes-item/recipes-item.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipes-item',
@@ -9,14 +8,15 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipesItemComponent implements OnInit {
  @Input() recipe: Recipe;
+ @Output() recipeSelected = new EventEmitter<void>();
 
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
   }
 
   onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
+    this.recipeSelected.emit();
   }
 
 }
